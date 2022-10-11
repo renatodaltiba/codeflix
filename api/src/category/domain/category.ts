6867c6ee8d26fd63e4b1995dec7192bf74d56e10
Renato Daltiba Schneider <renatodaltiba@gmail.com>
2022-10-10 21:28:10 -0300

    refactor: :recycle: refactored module to accept uuid

--- api/src/category/domain/category.ts
+++ api/src/category/domain/category.ts
@@ -1,4 +1,4 @@
-import {v4 as uuid} from 'uuid'
+import UniqueEntityId from "../../@shared/domain/unique-entity-id.vo";
 
 export type CategoryProperties = {
   name: string;
@@ -8,12 +8,11 @@ export type CategoryProperties = {
 }
 
 export class Category {
-  constructor (public readonly props: CategoryProperties, public readonly id?: string) {
-    this.id = id || uuid()
+  constructor (public readonly props: CategoryProperties, public readonly id?: UniqueEntityId) {
+    this.id = id || new UniqueEntityId()
     this.description = props.description
     this.is_active = props.is_active
     this.props.created_at = props.created_at ?? new Date()
-
   }
   get name () {
     return this.props.name
